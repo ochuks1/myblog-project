909ff9f5478bbccff589d916b17d39f4445bb64d
ochuks1 <cochuks@gmail.com>
2025-01-31 10:57:01 +0100

    README.md from create-react-app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import CreatePost from './components/CreatePost';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import CreatePost from './components/CreatePost';
+import Navbar from './components/Navbar';
+import NotFound from './components/NotFound'; // Ensure this is imported
+import './App.css';
 
 const App = () => {
-    return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/create" element={<CreatePost />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/createpost" element={<CreatePost />} />
+          {/* 404 Route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
